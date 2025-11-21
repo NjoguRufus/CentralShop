4e7b89c0c37e671536147147822ffe90f8515178
NjoguRufus <njogurufus@users.noreply.github.com>
2025-11-21 12:14:54 +0300

    going good

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,9 @@ export default defineConfig(({ mode }) => ({
         start_url: "/",
         scope: "/",
         icons: [
-          { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png", purpose: "any" },
-          { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png", purpose: "any" },
-          { src: "/icons/icon-maskable-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
+          { src: "/icons/CentalDarkmode.png", sizes: "192x192", type: "image/png", purpose: "any" },
+          { src: "/icons/CentalDarkmode.png", sizes: "512x512", type: "image/png", purpose: "any" },
+          { src: "/icons/CentalDarkmode.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
         ]
       },
       workbox: {
